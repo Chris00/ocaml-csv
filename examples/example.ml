728c3b1b8115216eee06417de060270db9560160
Samuel Hym <samuel.hym@gmail.com>
2013-05-10 00:27:08 +0200

    Illustrate the use of "of_string" in example.ml
    Using the data in example1.csv, illustrate how data can be parsed from
    a string.

--- examples/example.ml
+++ examples/example.ml
@@ -2,11 +2,37 @@
 
 open Printf
 
+let embedded_csv = "\
+\"Banner clickins\"
+\"Clickin\",\"Number\",\"Percentage\",
+\"brand.adwords\",\"4,878\",\"14.4\"
+\"vacation.advert2.adwords\",\"4,454\",\"13.1\"
+\"affiliates.generic.tc1\",\"1,608\",\"4.7\"
+\"brand.overture\",\"1,576\",\"4.6\"
+\"vacation.cheap.adwords\",\"1,515\",\"4.5\"
+\"affiliates.generic.vacation.biggestchoice\",\"1,072\",\"3.2\"
+\"breaks.no-destination.adwords\",\"1,015\",\"3.0\"
+\"fly.no-destination.flightshome.adwords\",\"833\",\"2.5\"
+\"exchange.adwords\",\"728\",\"2.1\"
+\"holidays.cyprus.cheap\",\"574\",\"1.7\"
+\"travel.adwords\",\"416\",\"1.2\"
+\"affiliates.vacation.generic.onlinediscount.200\",\"406\",\"1.2\"
+\"promo.home.topX.ACE.189\",\"373\",\"1.1\"
+\"homepage.hp_tx1b_20050126\",\"369\",\"1.1\"
+\"travel.agents.adwords\",\"358\",\"1.1\"
+\"promo.home.topX.SSH.366\",\"310\",\"0.9\""
+
+
 let csvs =
   List.map (fun name -> name, Csv.load name)
     [ "example1.csv"; "example2.csv" ]
 
+
 let () =
+  let ic = Csv.of_string embedded_csv in
+    printf "---Embedded CSV---------------------------------\n" ;
+    Csv.print_readable (Csv.input_all ic) ;
+
   List.iter (
     fun (name, csv) ->
       printf "---%s----------------------------------------\n" name;
