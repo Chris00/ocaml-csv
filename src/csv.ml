2d54af24f3ba8dd697baa9139144bf45bee8ee9b
Christophe Troestler <Christophe.Troestler@umons.ac.be>
2013-11-06 21:47:16 +0100

    Redefine the necessary List functions to be tail rec.

--- src/csv.ml
+++ src/csv.ml
@@ -47,6 +47,88 @@ type t = string list list
 (* Specialize to int for speed *)
 let max i j = if (i:int) < j then j else i
 
+(* Enhance the List module with tail rec functions. *)
+module List = struct
+  include List
+
+  (* Implementation of [map] in JSC Core. *)
+  let map_slow l ~f = List.rev (List.rev_map f l)
+
+  let rec count_map ~f l ctr =
+    match l with
+    | [] -> []
+    | [x1] ->
+       let f1 = f x1 in
+       [f1]
+    | [x1; x2] ->
+       let f1 = f x1 in
+       let f2 = f x2 in
+       [f1; f2]
+    | [x1; x2; x3] ->
+       let f1 = f x1 in
+       let f2 = f x2 in
+       let f3 = f x3 in
+       [f1; f2; f3]
+    | [x1; x2; x3; x4] ->
+       let f1 = f x1 in
+       let f2 = f x2 in
+       let f3 = f x3 in
+       let f4 = f x4 in
+       [f1; f2; f3; f4]
+    | x1 :: x2 :: x3 :: x4 :: x5 :: tl ->
+       let f1 = f x1 in
+       let f2 = f x2 in
+       let f3 = f x3 in
+       let f4 = f x4 in
+       let f5 = f x5 in
+       f1 :: f2 :: f3 :: f4 :: f5 :: (if ctr > 1000
+                                      then map_slow ~f tl
+                                      else count_map ~f tl (ctr + 1))
+
+  let map f l = count_map ~f l 0
+
+  (* Implementation of [append] in JSC core. *)
+  let slow_append l1 l2 = List.rev_append (List.rev l1) l2
+
+  let rec count_append l1 l2 count =
+    match l1 with
+    | []               ->                         l2
+    | [x1]             -> x1                   :: l2
+    | [x1; x2]         -> x1 :: x2             :: l2
+    | [x1; x2; x3]     -> x1 :: x2 :: x3       :: l2
+    | [x1; x2; x3; x4] -> x1 :: x2 :: x3 :: x4 :: l2
+    | x1 :: x2 :: x3 :: x4 :: x5 :: tl ->
+       x1 :: x2 :: x3 :: x4 :: x5 :: (if count > 1000
+                                      then slow_append tl l2
+                                      else count_append tl l2 (count + 1))
+
+  let append l1 l2 = count_append l1 l2 0
+
+  (* Tail recursive [combine]. *)
+  let rec rev_combine acc l1 l2 =
+    match l1, l2 with
+    | ([], []) -> acc
+    | (a1::l1, a2::l2) -> rev_combine ((a1, a2) :: acc) l1 l2
+    | (_, _) -> invalid_arg "List.combine"
+
+  let slow_combine l1 l2 = List.rev (rev_combine [] l1 l2)
+
+  let rec count_combine l1 l2 count =
+    match l1, l2 with
+    | ([], []) -> []
+    | ([x1], [y1]) -> [x1, y1]
+    | ([x1; x2], [y1; y2]) -> [x1, y1; x2, y2]
+    | ([x1; x2; x3], [y1; y2; y3]) -> [x1, y1; x2, y2; x3, y3]
+    | ([x1; x2; x3; x4], [y1; y2; y3; y4]) -> [x1, y1; x2, y2; x3, y3; x4, y4]
+    | (x1 :: x2 :: x3 :: x4 :: tl1), (y1 :: y2 :: y3 :: y4 :: tl2) ->
+       (x1, y1) :: (x2, y2) :: (x3, y3) :: (x4, y4)
+       :: (if count > 1000 then slow_combine tl1 tl2
+           else count_combine tl1 tl2 (count + 1))
+    | (_, _) -> invalid_arg "List.combine"
+
+  let combine l1 l2 = count_combine l1 l2 0
+
+end
 
 class type in_obj_channel =
 object
@@ -559,12 +641,12 @@ let rec dropwhile f = function
   | xs -> xs
 
 
+let rec empty_row = function
+  | [] -> true
+  | "" :: xs -> empty_row xs
+  | _ :: _ -> false
+
 let trim ?(top=true) ?(left=true) ?(right=true) ?(bottom=true) csv =
-  let rec empty_row = function
-    | [] -> true
-    | x :: _ when x <> "" -> false
-    | _ :: xs -> empty_row xs
-  in
   let csv = if top then dropwhile empty_row csv else csv in
   let csv =
     if right then
