17f942ef0f137533949e1dbcb9ce940e35cdc04b
Christophe Troestler <Christophe.Troestler@umons.ac.be>
2015-04-10 16:03:20 +0200

    Treat strings as immutable (safe-string)

--- src/csv.mli
+++ src/csv.mli
@@ -37,7 +37,7 @@ type t = string list list
 (** The most basic input object for best interoperability. *)
 class type in_obj_channel =
 object
-  method input : string -> int -> int -> int
+  method input : Bytes.t -> int -> int -> int
     (** [input buf ofs len] reads up to [len] octets from the channel
         and puts them in the substring [buf.[ofs .. ofs+len-1]].
         Returns the number of octets actually read (and stored).  When
@@ -52,7 +52,7 @@ end
 (** The most basic output object for best interoperability. *)
 class type out_obj_channel =
 object
-  method output : string -> int -> int -> int
+  method output : Bytes.t -> int -> int -> int
     (** [output s ofs len] writes up to [len] bytes of the substring
         [s.[ofs .. ofs+len-1]].  Return the number of bytes actually
         written.  When the channel is non-blocking, and there are
