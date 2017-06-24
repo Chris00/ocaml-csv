b4d08845154992035a16e473bab912fc114d6912
Christophe Troestler <Christophe.Troestler@umons.ac.be>
2017-06-24 12:01:13 +0200

    Execute the tests only for OCaml ≥ 4.02
    Some tests use quoted strings as well as __LINE__.

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -18,5 +18,18 @@ eval `opam config env`
 opam install $OPAM_PKGS
 
 export OCAMLRUNPARAM=b
-make
-make test
+oasis setup
+
+OCAML_VERSION=`ocamlc -version`
+OCAML_MAJOR=`echo $OCAML_VERSION | cut -d '.' -f 1`
+OCAML_MINOR=`echo $OCAML_VERSION | cut -d '.' -f 2`
+
+if test $OCAML_MAJOR -gt 4  -o \
+	\( $OCAML_MAJOR -eq 4 -a $OCAML_MINOR -ge 2 \); then
+    ocaml setup.ml -configure --enable-tests --enable-lwt
+    ocaml setup.ml -build
+    ocaml setup.ml -test
+else
+    ocaml setup.ml -configure --disable-tests --enable-lwt
+    ocaml setup.ml -build
+fi
