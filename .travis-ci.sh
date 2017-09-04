bd6a510af9a6804183b36bab6fdb9f4177c2b14b
Christophe Troestler <Christophe.Troestler@umons.ac.be>
2017-09-04 03:26:41 +0200

    Update travis script

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,6 +1,3 @@
-
-OPAM_PKGS="oasis base-bytes lwt"
-
 export OPAMYES=1
 
 if [ -f "$HOME/.opam/config" ]; then
@@ -15,21 +12,8 @@ if [ -n "${OPAM_SWITCH}" ]; then
 fi
 eval `opam config env`
 
-opam install $OPAM_PKGS
-
 export OCAMLRUNPARAM=b
-oasis setup
 
-OCAML_VERSION=`ocamlc -version`
-OCAML_MAJOR=`echo $OCAML_VERSION | cut -d '.' -f 1`
-OCAML_MINOR=`echo $OCAML_VERSION | cut -d '.' -f 2`
+opam pin -y add csv .
+opam pin -y add csv-lwt .
 
-if test $OCAML_MAJOR -gt 4  -o \
-	\( $OCAML_MAJOR -eq 4 -a $OCAML_MINOR -ge 2 \); then
-    ocaml setup.ml -configure --enable-tests --enable-lwt
-    ocaml setup.ml -build
-    ocaml setup.ml -test
-else
-    ocaml setup.ml -configure --disable-tests --enable-lwt
-    ocaml setup.ml -build
-fi
