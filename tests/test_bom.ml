087e23721e5e47824f81c09e9293d6b1859a2e34
Sébastien Dailly <sebastien@dailly.me>
2022-06-15 19:05:10 +0200

    Added a test for the check_bom parameter

---
+++ tests/test_bom.ml
@@ -0,0 +1,30 @@
+open Printf
+
+let print =
+  let print_assoc row =
+    List.iter (fun (k, v) -> printf " (%s, %s)" k v) row;
+    printf "\n"
+  in
+  fun csv -> List.iter print_assoc csv
+
+let testcsv ?check_bom filename expected =
+  try
+    let csv = Csv.Rows.load ?check_bom ~separator:';' filename in
+    let csv = List.map Csv.Row.to_assoc csv in
+    if csv <> expected then (
+      printf "input file: %s\n" filename;
+      printf "Csv library produced:\n";
+      print csv;
+      printf "Expected:\n";
+      print expected;
+      failwith "failed")
+  with Csv.Failure (nrow, nfield, err) ->
+    printf
+      "The file %S line %i, field %i, does not conform to the CSV \
+       specifications: %s\n"
+      filename nrow nfield err;
+    failwith "failed"
+
+let () =
+  testcsv "testcsv13.csv" ~check_bom:true
+    [ [ ("", "A\n"); ("", "B\n") ]; [ ("", "1"); ("", "2") ] ]
